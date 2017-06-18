3cf9a526001abb9417d6f80f0d0160d8cf16706f
DESKTOP-NEHV21A\Glenn <glenn.hoff@hva.nl>
2017-06-18 13:16:46 +0200

    Initial commit

---
+++ Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public Transform plaform;
+    public Transform jumpTrap;
+    public Transform duckTrap;
+    public Transform end; 
+
+    public int platformCount;
+    private int _platformMargin = 15;
+    public int startingPointX = -100;
+    private int _randomNumber;
+    private int _normalPlatformProbability = 60;
+
+    // Use this for initialization
+    void Start () {
+
+            for (int x = 0; x < platformCount; x++)
+            {
+            if(x == platformCount -1)
+            {
+                Instantiate(end, new Vector3(startingPointX + _platformMargin + 130, 0, 0), Quaternion.identity);
+                break;
+            }
+            _randomNumber = Random.Range(0, 100);
+
+            if(_randomNumber <= _normalPlatformProbability)
+            {
+                Instantiate(plaform, new Vector3(startingPointX + _platformMargin, 0, 0), Quaternion.identity);
+            } else if(_randomNumber <= 100 && _randomNumber >= 60)
+            {
+                int rand = Random.Range(0, 100);
+                if(rand <= 50)
+                {
+                    Instantiate(jumpTrap, new Vector3(startingPointX + _platformMargin, -0.1f, 0), Quaternion.identity);
+                } else if (rand <= 100 && rand >= 50)
+                {
+                    Instantiate(duckTrap, new Vector3(startingPointX + _platformMargin + 50, 0, 0), Quaternion.identity);
+                }
+                
+            }
+
+  
+            _platformMargin += 10;
+            }
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int CalculateProbabilityWithUserInput(int pProbability, int pUserScore, bool pChallanging, bool pFun)
+    {
+    
+        
+        if (pChallanging == false && pFun == false) {
+            pUserScore *= 5;
+            pProbability -= pUserScore;
+        }
+        else if (pChallanging == false && pFun == true)
+        {
+            pUserScore *= 5;
+            pProbability += pUserScore;
+        }
+        else if (pChallanging == true && pFun == true)
+        {
+            pUserScore *= 1;
+            pProbability += pUserScore;
+        }
+        else if (pChallanging == true && pFun == false)
+        {
+            pUserScore *= 1;
+            pProbability -= pUserScore;
+        }
+
+        return pProbability;
+    }
+
+}
